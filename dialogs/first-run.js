5e8acdff70f999f8fec596b34fc449c16a8ff74e
Oscar Mendoza <oscar@smartwave.ph>
2016-12-15 15:04:47 +0800

    Logging actions and repsonse on first=run dialog

--- dialogs/first-run.js
+++ dialogs/first-run.js
@@ -1,33 +1,34 @@
 var builder = builder = require('botbuilder'),
 	partyBot = require('partybot-http-client'),
 	Constants = require('../constants');
-
+const CL = console.log;
 module.exports = [
     function (session) {
+        CL(session.message.address);
         var params = {
         	organisationId: Constants.ORGANISATION_ID
         }
         partyBot.sender.getSender(params, function(error, response, body) {
-
+        	CL("Getting User");
         	if(!error && body.length > 0) {
-
-
-
-
+        		CL("Sender found");
+        		CL(error);
+        		CL(response.statusCode);
+        		CL(body);
 
         		session.beginDialog('/');
         	} else {
-
+        		CL("Sender not found");
         		var createParams = {
         			organisationId: Constants.ORGANISATION_ID,
         			sender: session.message.address.user.id,
         			channel_id: session.message.address.channelId
         		};
-
+        		CL("Creating sender");
         		partyBot.sender.createSender(createParams, function(err, res, bod){
-
-
-
+        			CL(err);
+        			CL(res.statusCode);
+        			CL(bod);
         		});
         		session.beginDialog('/');
         	}
