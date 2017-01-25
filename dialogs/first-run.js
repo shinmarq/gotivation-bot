7fa7832aa81eee8e804b15b444d8b63215c330f6
Oscar Mendoza <oscar@smartwave.ph>
2017-01-25 17:50:52 +0800

    Updating sender HTTP Client V66

--- dialogs/first-run.js
+++ dialogs/first-run.js
@@ -11,20 +11,25 @@ module.exports = [
 
         	if(!error && response.statusCode == 200) {
 
-        		session.beginDialog('/');
+                var updateParams = {
+                    organisationId: Constants.ORGANISATION_ID,
+                    sender: body._id,
+                    facebook_page_access_token: Constants.FB_PAGE_ACCESS_TOKEN
+                };
+                partyBot.sender.updateSender(updateParams, function(err, res, bod){
+                    session.beginDialog('/');
+                });
+        		
         	} else {
         		var createParams = {
         			organisationId: Constants.ORGANISATION_ID,
         			sender: session.message.address.user.id,
         			channel: session.message.address.channelId,
-                    facebook_page_access_token: [Constants.FB_PAGE_ACCESS_TOKEN]
+                    facebook_page_access_token: Constants.FB_PAGE_ACCESS_TOKEN
         		};
-        		partyBot.sender.createSender(createParams, function(err, res, bod){
-                    console.log(err);
-                    console.log(res.statusCode);
-                    console.log(bod);
-        		});
-        		session.beginDialog('/');
+                partyBot.sender.createSender(createParams, function(err, res, bod){
+                    session.beginDialog('/');
+                });
         	}
         });        
     }
