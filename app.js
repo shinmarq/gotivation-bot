bde417dd90bc05692f16277136f22add25d84506
Oscar Mendoza <oscar@smartwave.ph>
2017-02-07 16:59:15 +0800

    New First Run

--- app.js
+++ app.js
@@ -147,12 +147,38 @@ bot.use({
             function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     session.userData.firstRun = true;
-                    session.send(`Hi ${session.message.address.user.name} Welcome to the official The Palace Messenger Bot! I’m here to make your partying easier! If you want to find out all the things I can do for you, type “Menu”`);
+                    var img = new builder.Message(session)
+                    .addAttachment({
+                        contentUrl: `${CONSTANTS.BASE_URL}/assets/logo.jpg`,
+                        contentType: 'images'
+                    });
+                    session.send(img);
+                    session.send(`Hi ${session.message.address.user.name} Welcome to the official The Palace Messenger Bot! I’m here to make your partying easier! Click the button below to start!`);
+                    var card = new builder.HeroCard(session)
+                    .buttons([
+                        builder.CardAction.imBack(session, 'menu', 'Main Menu')
+                        ]);
+                    var msg = new builder.Message(session).addAttachment(card);
+                    session.send(msg);
+
                     session.beginDialog('/firstRun');
                     next();
                 } else { 
                     session.userData.firstRun = true;
-                    session.send(`Hi ${session.message.address.user.name} Welcome to the official The Palace Messenger Bot! I’m here to make your partying easier! If you want to find out all the things I can do for you, type “Menu”`);
+                    var img = new builder.Message(session)
+                    .addAttachment({
+                        contentUrl: `${CONSTANTS.BASE_URL}/assets/logo.jpg`,
+                        contentType: 'images'
+                    });
+                    session.send(img);
+                    session.send(`Hi ${session.message.address.user.name} Welcome to the official The Palace Messenger Bot! I’m here to make your partying easier! Click the button below to start!`);
+                    var card = new builder.HeroCard(session)
+                    .buttons([
+                        builder.CardAction.imBack(session, 'menu', 'Main Menu')
+                        ]);
+                    var msg = new builder.Message(session).addAttachment(card);
+                    session.send(msg);
+
                     session.beginDialog('/firstRun');
                     next();
                 }
