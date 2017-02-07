ccd080d3f56231c8c5278736f009e92416d0c58e
Oscar Mendoza <oscar@smartwave.ph>
2017-02-07 19:44:01 +0800

    Menu

--- console.js
+++ console.js
@@ -133,35 +133,35 @@ bot.use({
             function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     session.userData.firstRun = true;
+                    var img = new builder.Message(session)
+                    .addAttachment({
+                        contentUrl: `${CONSTANTS.BASE_URL}/assets/logo.jpg`,
+                        contentType: 'image/jpg'
+                    });
+                    session.send(img);
                     session.send(`Hi ${session.message.address.user.name} Welcome to the official The Palace Messenger Bot! I’m here to make your partying easier! Click the button below to start!`);
-                    var card = new builder.ThumbnailCard(session)
-                    .title(null)
-                    .images([
-                        builder.CardImage.create(session, `${CONSTANTS.BASE_URL}/logo.jpg`)
-                        ])
+                    var card = new builder.HeroCard(session)
                     .buttons([
                         builder.CardAction.imBack(session, 'menu', 'Main Menu')
                         ]);
-                    var msg = new builder.Message(session)
-                    .attachmentLayout(builder.AttachmentLayout.carousel)
-                    .addAttachment(card);
+                    var msg = new builder.Message(session).addAttachment(card);
                     session.send(msg);
                     session.beginDialog('/firstRun');
                     next();
                 } else {
                     session.userData.firstRun = true;
+                    var img = new builder.Message(session)
+                    .addAttachment({
+                        contentUrl: `${CONSTANTS.BASE_URL}/assets/logo.jpg`,
+                        contentType: 'image/jpg'
+                    });
+                    session.send(img);
                     session.send(`Hi ${session.message.address.user.name} Welcome to the official The Palace Messenger Bot! I’m here to make your partying easier! Click the button below to start!`);
-                    var card = new builder.ThumbnailCard(session)
-                    .title(null)
-                    .images([
-                        builder.CardImage.create(session, `${CONSTANTS.BASE_URL}/logo.jpg`)
-                        ])
+                    var card = new builder.HeroCard(session)
                     .buttons([
                         builder.CardAction.imBack(session, 'menu', 'Main Menu')
                         ]);
-                    var msg = new builder.Message(session)
-                    .attachmentLayout(builder.AttachmentLayout.carousel)
-                    .addAttachment(card);
+                    var msg = new builder.Message(session).addAttachment(card);
                     session.send(msg);
                     session.beginDialog('/firstRun');
                     next();
