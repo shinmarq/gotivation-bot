2b57cd062c0e4c3f96829d85cb0606da691c6589
Bench <bench@chatbot.ph>
2017-03-29 12:59:09 +0800

    test

--- app.js
+++ app.js
@@ -200,19 +200,25 @@ function addPersistentMenu(){
         setting_type : "call_to_actions",
         thread_state : "existing_thread",
         call_to_actions:[
-            // {
-            //   type:"postback",
-            //   title:"Menu",
-            //   payload:"menu"
-            // },
+            
             {
               type:"postback",
               title:"Restart Bot",
               payload:"GET_STARTED"
             },
             {
+              type:"postback",
+              title:"Table Booking",
+              payload:"menu"
+            },
+            {
+              type:"postback",
+              title:"Guest List",
+              payload:"menu"
+            },
+            {
               type:"web_url",    
-              title:"Powered by ChatbotPH",
+              title:"Powered by Partybot",
               url:"http://partybot.rocks/"
             }
           ]
