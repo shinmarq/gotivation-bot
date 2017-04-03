03f871d2ffadf8060bbe9ce072e2d889dae902c7
Bench <bench@chatbot.ph>
2017-04-03 12:00:24 +0800

    keywords

--- dialogs/default.js
+++ dialogs/default.js
@@ -24,13 +24,57 @@ module.exports = [
             else if((/^book table|book a table|table book|table booking|table|couch/i.test(entity))){
                 session.beginDialog('/book-table');
             }
-            else if((/^buy tickets|ticket|buy ticket|tickets/i.test(entity))){
+            else if((/^buy tickets|ticket|buy ticket|tickets|tix/i.test(entity))){
                 session.beginDialog('/buy-tickets');
             }
             else if((/:/i.test(entity)))
             {
                 session.send("Let's start from the menu again.")
                 session.beginDialog('/menu');
+            }
+            else if((/^address|location/i.test(entity)))
+            {
+                session.send("We're located at 11th Avenue corner 38th Street Uptown Bonifacio, Taguig City")
+            }
+            else if((/^open|operating hours|time/i.test(entity)))
+            {
+                session.send("The Palace Pool Club is open all days of the week from 11AM to 4AM. Valkyrie is open Wednesdays - Saturdays from 10PM - 4AM. Revel is open Mondays - Saturdays from 10PM - 5AM.")
+            }
+            else if((/^payment|card|credit/i.test(entity)))
+            {
+                session.send("We can accept cash or credit card payment only. We dont accept cheques or bank deposits as of yet.")
+            }
+            else if((/^vacancies|job|opening|openings/i.test(entity)))
+            {
+                session.send("You may send your resume to careers@thepalacemanila.com")
+            }
+            else if((/^number|hotline/i.test(entity)))
+            {
+                session.send("You may contact us through the following numbers: Valkyrie - 09176808888, Pool Club - 09176898888, Revel - 09175508888")
+            }
+            else if((/^school id|id/i.test(entity)))
+            {
+                session.send("As long as your ID has your birthdate, your picture, and full name in it then it's fine")
+            }
+            else if((/^door charge|charge|fee|entrance fee/i.test(entity)))
+            {
+                session.send("Door charge is P800 inclusive of 2 drinks already :)")
+            }
+            else if((/^photos/i.test(entity)))
+            {
+                session.send("Photos are posted on our Facebook pages! Please check Valkyrie, Pool Club and Revel Facebook pages.")
+            }
+            else if((/^thanks|thank/i.test(entity)))
+            {
+                session.send("You're welcome!")
+            }
+            else if((/^tawesome|cool|wow/i.test(entity)))
+            {
+                session.send("I know right? :D")
+            }
+            else if((/^dress code|dress|dresscode/i.test(entity)))
+            {
+                session.send("Dress code is smart casual.")
             }
             else {
                var params = {
