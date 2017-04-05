1ee182c4e6b919e2540e30c0b481b3fa86947840
Bench <bench@chatbot.ph>
2017-04-05 17:37:42 +0800

    Last hope

--- app.js
+++ app.js
@@ -7,18 +7,8 @@ var async = require('async');
 var _ = require('underscore');
 var partyBot = require('partybot-http-client');
 var request = require('request');
-var ORGANISATION_ID = "5800471acb97300011c68cf7";
-var FBPAGE_ACCESS_TOKEN = "EAANW2ZALpyZAABALnAf7FTmhOgrciIkZBBvLjH8o8gpC5m1NzBWW5xbDstkCOq8TR8ZBNsJfwHjeaUsxZBaYESyxGew1BrzkippXM8vIFHeDbvraHw59Xj4QNrrZBpreBkE7cJ1SGTIPjcBXq4e3CedZBHU6wJV3ZCfARxAZAeR438gZDZD";
-
 const util = require('util');
 const CONSTANTS = require('./constants');
-var Menu = require('./dialogs/menu'),
-    GuestList = require('./dialogs/guest-list'),
-    BuyTicket = require('./dialogs/buy-ticket'),
-    BookTable = require('./dialogs/book-table'),
-    DefaultDiag = require('./dialogs/default'),
-    FirstRun = require('./dialogs/first-run'),
-    EnsurePromoterCode = require('./dialogs/ensure-promoter-code');
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -41,87 +31,6 @@ var connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-
-var fburl ='https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAANW2ZALpyZAABANrZAuKgOkZC69lsLkziaA6wsNEMOZAqRgBzguyGvJEkCa7mfA7nw6ewlJq5cHdUytcBqz5YwhcZCDmPPdI12hTh48yjhwOULtIm9yokJ8bm7BUbmZAPALIwXlev1g6mcmWveWZCCjO7bXgFOA5hqtOvjZBPWtSZCwZDZD'
-//=========================================================
-// AI Setup
-//=========================================================
-
-// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
-var model = process.env.model ||
-    'https://api.projectoxford.ai/luis/v1/application?id=6c4a0d3e-41ff-4800-9ec7-8fd206ee41e8&subscription-key=692f717f9c3b4f52b852d51c46358315&q=';
-var recognizer = new builder.LuisRecognizer(model)
-var intentDialog = new builder.IntentDialog({
-    recognizers: [recognizer],
-    intentThreshold: 0.5,
-    recognizeMode: builder.RecognizeMode.onBegin
-});
-
-//=========================================================
-// Activity Events
-//=========================================================
-bot.on('conversationUpdate', function (message) {
-    // Check for group conversations
-    if (message.address.conversation.isGroup) {
-        // Send a hello message when bot is added
-        if (message.membersAdded) {
-            message.membersAdded.forEach(function (identity) {
-                if (identity.id === message.address.bot.id) {
-                    var reply = new builder.Message()
-                        .address(message.address)
-                        .text("Hello everyone!");
-                    bot.send(reply);
-                }
-            });
-        }
-
-        // Send a goodbye message when bot is removed
-        if (message.membersRemoved) {
-            message.membersRemoved.forEach(function (identity) {
-                if (identity.id === message.address.bot.id) {
-                    var reply = new builder.Message()
-                        .address(message.address)
-                        .text("Goodbye");
-                    bot.send(reply);
-                }
-            });
-        }
-    }
-});
-
-bot.on('contactRelationUpdate', function (message) {
-    if (message.action === 'add') {
-        var name = message.user ? message.user.name : null;
-        var reply = new builder.Message()
-            .address(message.address)
-            .text("Hello %s... Welcome to the Official The Palace Messenger Bot! What can I do for you?", name || 'there');
-        bot.send(reply);
-    } else {
-        // delete their data
-    }
-});
-
-bot.on('deleteUserData', function (message) {
-    // User asked to delete their data
-});
-
-//=========================================================
-// Bots Middleware
-//=========================================================
-
-// Anytime the major version is incremented any existing conversations will be restarted.
-bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
-// bot.use(builder.Middleware.firstRun({ version: 1.0, dialogId: '/firstRun' }));
-
-//=========================================================
-// Bots Global Actions
-//=========================================================
-
-// bot.endConversationAction('goodbye', 'See you at The Palace!', { matches: /^goodbye/i });
-
-//=========================================================
-// Bots Dialogs
-//=========================================================// Set FB bot greeting text
 
 
 bot.use({
@@ -152,7 +61,7 @@ bot.use({
                     if (!error && response.statusCode == 200) {
                         session.userData.firstRun = true;
                         var welcomeCard = new builder.HeroCard(session)
-                            .title('Palace Messenger bot')
+                            .title('Gotivation bot')
                             .subtitle(`Wanna party tonight? Click Main Menu so I can help!`)
                             .images([
                                 new builder.CardImage(session)
@@ -165,7 +74,6 @@ bot.use({
 
                         session.send(new builder.Message(session)
                             .addAttachment(welcomeCard));
-
                         session.beginDialog('/firstRun');
                         next();
                     } else {
@@ -197,142 +105,7 @@ bot.use({
 });
 
 bot.dialog('/firstRun', FirstRun);
-bot.dialog('/', intentDialog);
-bot.dialog('/menu', Menu).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
 
-bot.dialog('/guest-list', GuestList); // End guest-list
-
-bot.dialog('/ensure-party', [
-    function (session, args, next) {
-        builder.Prompts.text(session, 'Please enter the names you would like to add in the guest list (one name per line or comma separated):');
-    },
-    function (session, results, next) {
-        if (results.response) {
-            session.dialogData.party = results.response.split(/[,\n]+/).map(function (x) { return x.trim(); }) || [];
-            builder.Prompts.confirm(session, `${session.dialogData.party.join('<br/>')}<br/>Is this confirmed?`);
-        }
-    },
-    function (session, results) {
-        var choice = results.response ? 'yes' : 'no';
-        if (choice === 'yes') {
-            session.endDialogWithResult(session.dialogData.party);
-        } else {
-            session.replaceDialog('/ensure-party');
-        }
-    }
-]);
-
-bot.dialog('/ensure-promoter-code', EnsurePromoterCode.Dialog);
-
-bot.dialog('/book-table', BookTable);
-
-bot.dialog('/ensure-table', [
-    function (session, args, next) {
-        // todo : verify table availability ("add to cart")
-        var verified = true;
-        if (verified) {
-
-        }
-    },
-    function (session, results, next) {
-
-    }
-]);
-
-bot.dialog('/buy-tickets', BuyTicket);
-
-//=========================================================
-// Natural Language Processing
-//=========================================================
-
-//=========================================================
-// Natural Language Processing
-//=========================================================
-
-// intentDialog.matches('Greet', [ 
-//     function (session, args, next) {
-//         var argsJSONString = JSON.stringify(args);
-//         // session.send(`Greet intent detected. ${argsJSONString}.`);
-//         session.send(`Hello.`);
-//         next();
-//     }
-// ]);
-
-// intentDialog.matches('AskSomething', [ 
-//     function (session, args, next) {
-//         var argsJSONString = JSON.stringify(args);
-//         // session.send(`AskSomething intent detected. ${argsJSONString}`);
-//         session.send(`Getting ready for tonight's craziness at The Palace! How about you?`);
-//         next();
-//     }
-// ]);
-
-// intentDialog.matches('Appreciate', [ 
-//     function (session, args, next) {
-//         var argsJSONString = JSON.stringify(args);
-//         session.send(`No problem :)`);
-//         // session.send(`Appreciate intent detected. ${argsJSONString}`);
-//         next();
-//     }
-// ]);
-
-// intentDialog.matches('Confirm', [ 
-//     function (session, args, next) {
-//         var argsJSONString = JSON.stringify(args);
-//         session.send(`Cool!`);
-//         // session.send(`Confirm intent detected. ${argsJSONString}`);
-//         next();
-//     }
-// ]);
-
-// intentDialog.matches('Negative', [ 
-//     function (session, args, next) {
-//         var argsJSONString = JSON.stringify(args);
-//         session.send(`Alright!`);
-//         // session.send(`Negative intent detected. ${argsJSONString}`);
-//         next();
-//     }
-// ]);
-
-// intentDialog.matches('Curse', [ 
-//     function (session, args, next) {
-//         var argsJSONString = JSON.stringify(args);
-//         session.send(`That's not a very nice thing to say :(`);
-//         // session.send(`Curse intent detected. ${argsJSONString}`);
-//         next();
-//     }
-// ]);
-
-// intentDialog.matches('Leave', [ 
-//     function (session, args, next) {
-//         var argsJSONString = JSON.stringify(args);
-//         session.send(`See you at The Palace!`);
-//         // session.send(`Leave intent detected. ${argsJSONString}`);
-//         next();
-//     }
-// ]);
-
-intentDialog.onDefault([
-    function (session, next) {
-        session.replaceDialog('/default', session.message.text);
-    }
-    // function (session, args, next) {
-    //     // Send a greeting and show the menu.
-    //     var card = new builder.HeroCard(session)
-    //     // todo: change to venue.model
-    //         .title("The Palace Bot")
-    //         .text("Official Bot of The Palace Manila")
-    //         .images([
-    //              builder.CardImage.create(session, "https://pbs.twimg.com/profile_images/522713296315486208/kZFy9pGU.jpeg")
-    //         ]);
-    //     var msg = new builder.Message(session).attachments([card]);
-    //     session.send(msg);
-    //     session.send("Welcome to the Official The Palace Messenger Bot!");
-    //     session.beginDialog('/menu');
-    // }
-    //
-]);
-// bot.dialog('/default', DefaultDiag);
 bot.dialog('/default', [
     function (session, args, next) {
         var entity = args || session.message.text;
@@ -344,64 +117,6 @@ bot.dialog('/default', [
            if((/^menu|show menu/i.test(entity))) {
                  session.beginDialog('/menu');
              }  
-            else if((/^guest list|guest|show guest list|gl/i.test(entity))){
-                session.beginDialog('/guest-list');
-            }
-            else if((/^book table|book a table|table book|table booking|table|couch/i.test(entity))){
-                session.beginDialog('/book-table');
-            }
-            else if((/^buy tickets|ticket|buy ticket|tickets|tix/i.test(entity))){
-                session.beginDialog('/buy-tickets');
-            }
-            else if((/:/i.test(entity)))
-            {
-                session.send("Let's start from the menu again.")
-                session.beginDialog('/menu');
-            }
-            else if((/^address|location/i.test(entity)))
-            {
-                session.send("We're located at 11th Avenue corner 38th Street Uptown Bonifacio, Taguig City")
-            }
-            else if((/^open|operating hours|time/i.test(entity)))
-            {
-                session.send("The Palace Pool Club is open all days of the week from 11AM to 4AM. Valkyrie is open Wednesdays - Saturdays from 10PM - 4AM. Revel is open Mondays - Saturdays from 10PM - 5AM.")
-            }
-            else if((/^payment|card|credit/i.test(entity)))
-            {
-                session.send("We can accept cash or credit card payment only. We dont accept cheques or bank deposits as of yet.")
-            }
-            else if((/^vacancies|job|opening|openings/i.test(entity)))
-            {
-                session.send("You may send your resume to careers@thepalacemanila.com")
-            }
-            else if((/^number|hotline/i.test(entity)))
-            {
-                session.send("You may contact us through the following numbers: Valkyrie - 09176808888, Pool Club - 09176898888, Revel - 09175508888")
-            }
-            else if((/^school id|id/i.test(entity)))
-            {
-                session.send("As long as your ID has your birthdate, your picture, and full name in it then it's fine")
-            }
-            else if((/^door charge|charge|fee|entrance fee/i.test(entity)))
-            {
-                session.send("Door charge is P800 inclusive of 2 drinks already :)")
-            }
-            else if((/^photos/i.test(entity)))
-            {
-                session.send("Photos are posted on our Facebook pages! Please check Valkyrie, Pool Club and Revel Facebook pages.")
-            }
-            else if((/^thanks|thank/i.test(entity)))
-            {
-                session.send("You're welcome!")
-            }
-            else if((/^awesome|cool|wow/i.test(entity)))
-            {
-                session.send("I know right? :D")
-            }
-            else if((/^dress code|dress|dresscode/i.test(entity)))
-            {
-                session.send("Dress code is smart casual.")
-            }
             else {
                 var params = {
                     organisationId: ORGANISATION_ID,
@@ -414,7 +129,6 @@ bot.dialog('/default', [
                             'In the mean time, type “Menu” if you want to find out the cool things I can do for you!');
                         // session.replaceDialog('/menu');
                         var createParams = {
-                            organisationId: ORGANISATION_ID,
                             entity: entity,
                             _venue_id: null 
                             
