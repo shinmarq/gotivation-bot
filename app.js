cb6e0d03fd97f5f04ce3c9a94a4e2d6790c9cebc
Bench <bench@chatbot.ph>
2017-04-17 12:59:21 +0800

    first name push 4

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ bot.use({
                             .addAttachment(welcomeCard));
 
                         session.sendTyping();
-                        session.send(`Hi ${session.message.address.user.name.first_name}!,Welcome to GOtivation! Together, we’re going to motivate, educate, and encourage you along our fitness journey. Each day, I’ll send you motivation that is scientifically proven to help you succeed. I think you’re going to be excited about the transformation :)`)
+                        session.send(`Hi ${session.message.address.user.first_name}! Welcome to GOtivation! Together, we’re going to motivate, educate, and encourage you along our fitness journey. Each day, I’ll send you motivation that is scientifically proven to help you succeed. I think you’re going to be excited about the transformation :)`)
                         session.beginDialog('/get-coachcode');
                     }
                 });
