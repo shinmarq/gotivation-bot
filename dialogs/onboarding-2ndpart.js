7a92231776a35d3006a474ef82858231f273b803
Shin <shin@chatbot.ph>
2017-05-12 16:49:06 +0800

    test

--- dialogs/onboarding-2ndpart.js
+++ dialogs/onboarding-2ndpart.js
@@ -18,7 +18,7 @@ module.exports = [
         if (results.response) {
             // session.dialogData.recurrence = builder.EntityRecognizer.resolveTime([results.response]);
             var recurrence = builder.EntityRecognizer.resolveTime([results.response]);
-            console.log(recurrence);
+            console.log(recurrence.getUTCHours());
             recurrence = recurrence.getUTCHours() +':'+ recurrence.getUTCMinutes();
             
             session.dialogData.recurrence = recurrence;
