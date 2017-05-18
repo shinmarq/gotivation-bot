adff02df31bc2ad7511f025625545f9a86dd057f
Shin <shin@chatbot.ph>
2017-05-18 19:01:01 +0800

    test

--- dialogs/onboarding-1stpart.js
+++ dialogs/onboarding-1stpart.js
@@ -127,26 +127,27 @@ module.exports = [
         }
         else {
 
-            async.waterfall([ async.apply( _function1, _function2)], 
-            function (err, result) {
-                if(err){ 
-                    console.error(err); 
-                    return;
-                }
+            // async.waterfall([ async.apply( _function1, _function2)], 
+            // function (err, result) {
+            //     if(err){ 
+            //         console.error(err); 
+            //         return;
+            //     }
                  
-                console.log(result);
-            });
+            //     console.log(result);
+            // });
 
-            function _function1(callback) {
-                session.replaceDialog('/member-session', session.dialogData);
-                callback(null, 'func1');
-            }
+            // function _function1(callback) {
+            //     session.replaceDialog('/member-session', session.dialogData);
+            //     callback(null, 'func1');
+            // }
 
-            function _function2(arg, callback) { 
-                session.replaceDialog('/onboarding-2ndpart');
-                callback(null, 'func2');
-            }
-
+            // function _function2(arg, callback) { 
+            //     session.replaceDialog('/onboarding-2ndpart');
+            //     callback(null, 'func2');
+            // }
+            session.replaceDialog('/member-session', session.dialogData);
+            session.replaceDialog('/onboarding-2ndpart');
             
         }
     }
