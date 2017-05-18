16f05c820f8d040e7e523eed530eac3ed1c528f1
Shin <shin@chatbot.ph>
2017-05-18 11:37:40 +0800

    test

--- dialogs/onboarding-1stpart.js
+++ dialogs/onboarding-1stpart.js
@@ -128,14 +128,15 @@ module.exports = [
         else {
 
             async.waterfall([
-                async.apply(_function1, _function2)  
-            ], function (error, result) {
-                !error ? console.log(result) :  console.log(error);
+                async.apply(_function1,_function2)   
+            ], function (error, success) {
+                if (error) { console.log('Something is wrong!'); }
+                console.log('Done!');
             });
 
             function _function1(callback) {
                 session.replaceDialog('/member-session', session.dialogData);
-                callback(null, 'success');
+                callback(null, session.dialogData);
             }
 
             function _function2(arg1,callback) {
