c4b3bb7894ede1d1440753bdadd182e6d8df5222
Bench <bench@chatbot.ph>
2017-05-19 13:40:11 +0800

    change bast path to herou

--- constants.js
+++ constants.js
@@ -4,12 +4,12 @@ module.exports.BASE_URL = (process.env.NODE_ENV !== "production")? "http://local
 
 module.exports.SUCCESS = 200;
 module.exports.CREATED = 201;
-module.exports.BASE_PATH = 'https://df19ea99.ngrok.io/';
-//module.exports.BASE_PATH = 'https://gotivation-api.herokuapp.com/';
+module.exports.BASE_PATH = 'https://gotivation-api.herokuapp.com/';
+//module.exports.BASE_PATH = 'https://df19ea99.ngrok.io/';
 
 module.exports.API_PATH = '/api';
 module.exports.CLOUDINARY_CLOUD_NAME = 'hobwovvya'; //change
 module.exports.CLOUDINARY_API_KEY = '954985777471899'; //change 
 module.exports.CLOUDINARY_API_SECRET = 'FZttVubZH32iXJkiaaB6Wso_edI'; //change
 
-module.exports.IMG_PATH = 'http://35.164.157.19/gtv_assets/';``
+module.exports.IMG_PATH = 'http://35.164.157.19/gtv_assets/';
