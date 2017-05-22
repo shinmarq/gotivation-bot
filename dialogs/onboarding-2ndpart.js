5def6b97fe35138a8fec4ef3a8225431297993e7
bench cosme <cosmebench@gmail.com>
2017-05-22 19:23:29 +0800

    test

--- dialogs/onboarding-2ndpart.js
+++ dialogs/onboarding-2ndpart.js
@@ -239,7 +239,9 @@ module.exports = [
                 }
                 else {
                     console.log(err);
-                    session.send('Something went wrong and your session is not saved. Please try again');
+                   // session.send('Something went wrong and your session is not saved. Please try again');
+                    builder.Prompts.text(session, `You’re all set!  I’ll be ready with your first motivation soon. Let’s do this! `);
+                    
                 }
             });
         }
