75d36ce53719b57f0948b9694227e05a02ed258b
bench cosme <cosmebench@gmail.com>
2017-05-22 18:40:41 +0800

    test no message

--- dialogs/onboarding-2ndpart.js
+++ dialogs/onboarding-2ndpart.js
@@ -203,6 +203,7 @@ module.exports = [
                 });
             session.send(msg);
             builder.Prompts.choice(session,
+                "",
                 options, {
                     listStyle: builder.ListStyle.button,
                     retryPrompt: `Your onboarding session will not be saved unless you agree with the Terms of service and Privacy Policy`
