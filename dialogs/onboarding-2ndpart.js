cef4e6d1240f74410ee19bf4e9879c9d6936f4f7
bench cosme <cosmebench@gmail.com>
2017-05-22 16:44:47 +0800

    test

--- dialogs/onboarding-2ndpart.js
+++ dialogs/onboarding-2ndpart.js
@@ -189,6 +189,28 @@ module.exports = [
                         "contentType": "application/vnd.microsoft.card.receipt",
                         "content": {
                             "title": "I'm a receipt card",
+                            "items": [
+                                {
+                                    "title": "Sushi",
+                                    "subtitle": "2 piece",
+                                    "image": {
+                                        "url": "https://mydeploy.azurewebsites.net/sushi.png"
+                                    },
+                                    "price": "16.25",
+                                    "quantity": "1"
+                                },
+                                {
+                                    "title": "Tenpura",
+                                    "subtitle": "1 dish",
+                                    "image": {
+                                        "url": "https://mydeploy.azurewebsites.net/tenpura.jpg"
+                                    },
+                                    "price": "34.50",
+                                    "quantity": "2"
+                                }
+                            ],
+                            "total": "275.25",
+                            "tax": "27.52",
                             "buttons": [
                                 {
                                     "type": "openUrl",
