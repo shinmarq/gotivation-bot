94e2ba2cc95fd3dad18d79c233e07826d89baa26
bench cosme <cosmebench@gmail.com>
2017-05-22 17:21:41 +0800

    try again wtf

--- dialogs/onboarding-2ndpart.js
+++ dialogs/onboarding-2ndpart.js
@@ -181,76 +181,8 @@ module.exports = [
     function (session, results, next) {
         if (results.response) {
             var msg = new builder.Message(session)
+                .text('By clicking "I Agree", you agree with our Terms of Service and Privacy Policy')
                 .addAttachment({
-                    contentType: "application/vnd.microsoft.card.adaptive",
-                    content: {
-                        type: "AdaptiveCard",
-                        speak: "<s>Your  meeting about \"Adaptive Card design session\"<break strength='weak'/> is starting at 12:30pm</s><s>Do you want to snooze <break strength='weak'/> or do you want to send a late notification to the attendees?</s>",
-                        body: [
-                            {
-                                "type": "TextBlock",
-                                "text": "Adaptive Card design session",
-                                "size": "large",
-                                "weight": "bolder"
-                            },
-                            {
-                                "type": "TextBlock",
-                                "text": "Conf Room 112/3377 (10)"
-                            },
-                            {
-                                "type": "TextBlock",
-                                "text": "12:30 PM - 1:30 PM"
-                            },
-                            {
-                                "type": "TextBlock",
-                                "text": "Snooze for"
-                            },
-                            {
-                                "type": "Input.ChoiceSet",
-                                "id": "snooze",
-                                "style": "compact",
-                                "choices": [
-                                    {
-                                        "title": "5 minutes",
-                                        "value": "5",
-                                        "isSelected": true
-                                    },
-                                    {
-                                        "title": "15 minutes",
-                                        "value": "15"
-                                    },
-                                    {
-                                        "title": "30 minutes",
-                                        "value": "30"
-                                    }
-                                ]
-                            }
-                        ],
-                        "actions": [
-                            {
-                                "type": "Action.Http",
-                                "method": "POST",
-                                "url": "http://foo.com",
-                                "title": "Snooze"
-                            },
-                            {
-                                "type": "Action.Http",
-                                "method": "POST",
-                                "url": "http://foo.com",
-                                "title": "I'll be late"
-                            },
-                            {
-                                "type": "Action.Http",
-                                "method": "POST",
-                                "url": "http://foo.com",
-                                "title": "Dismiss"
-                            }
-                        ]
-                    }
-                });
-            var msg = new builder.Message(session)
-                .addAttachment({
-                    text: `By clicking "I Agree", you agree with our Terms of Service and Privacy Policy`,
                     contentType: "application/vnd.microsoft.card",
                     content: {
                         "buttons": [
