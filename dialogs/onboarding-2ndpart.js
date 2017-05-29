fbc9fef89c01d168258a70a72f159bc06c303ed5
bench cosme <cosmebench@gmail.com>
2017-05-29 17:15:30 +0800

    test utc

--- dialogs/onboarding-2ndpart.js
+++ dialogs/onboarding-2ndpart.js
@@ -18,7 +18,9 @@ module.exports = [
         if (results.response) {
             // session.dialogData.recurrence = builder.EntityRecognizer.resolveTime([results.response]);
             var recurrence = builder.EntityRecognizer.resolveTime([results.response]);
-            recurrence = moment.utc(recurrence).format();
+            recurrence = moment.utc(recurrence);
+            session.send(recurrence);
+            
             recurrence = recurrence.getUTCHours() + ":" + recurrence.getUTCMinutes();
             session.send(recurrence);
             session.dialogData.recurrence = recurrence;
