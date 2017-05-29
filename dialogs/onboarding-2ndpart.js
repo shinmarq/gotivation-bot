b1c60eaf08692db09eb578a78ed66c4e1d8af184
bench cosme <cosmebench@gmail.com>
2017-05-29 20:15:21 +0800

    back to working

--- dialogs/onboarding-2ndpart.js
+++ dialogs/onboarding-2ndpart.js
@@ -18,11 +18,10 @@ module.exports = [
         if (results.response) {
             // session.dialogData.recurrence = builder.EntityRecognizer.resolveTime([results.response]);
             var recurrence = builder.EntityRecognizer.resolveTime([results.response]);
-            let offset = recurrence.getTimezoneOffset()/60;
-            console.log(offset);
+            
             let test = moment.utc(recurrence).format();
 
-            let newTest = moment(test).add(offset, "hours").format("HH:mm");
+            let newTest = moment(test).subtract(8, "hours").format("HH:mm");
             //console.log(newTest);
             // console.log(test);
             // var isoDate = new Date(recurrence).toISOString();
