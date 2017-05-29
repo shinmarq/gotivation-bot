3210173118bc20856a076fceedc7aa8acf6c3b98
bench cosme <cosmebench@gmail.com>
2017-05-29 17:25:11 +0800

    test remove utc

--- dialogs/onboarding-2ndpart.js
+++ dialogs/onboarding-2ndpart.js
@@ -19,8 +19,8 @@ module.exports = [
             // session.dialogData.recurrence = builder.EntityRecognizer.resolveTime([results.response]);
             var recurrence = builder.EntityRecognizer.resolveTime([results.response]);
             moment.utc(recurrence);
-            recurrence = recurrence.getUTCHours() + ":" + recurrence.getUTCMinutes();
-            session.send(recurrence);
+             recurrence = recurrence.getHours() + ":" + recurrence.getMinutes();
+             session.send(recurrence);
             session.dialogData.recurrence = recurrence;
             if (session.dialogData.recurrence) {
                 session.send("Got it! Please indicate how much the following statements describe you.");
