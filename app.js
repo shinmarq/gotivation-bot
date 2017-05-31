6789badbba8777d1d72c3b95852b8ea58f6ee799
Shin <shin@chatbot.ph>
2017-05-31 18:42:14 +0800

    test

--- app.js
+++ app.js
@@ -77,6 +77,7 @@ bot.use({
             },
                 function (error, response, body) {
                     if (!error && response.statusCode == 200) {
+                        console.log(session);
                     //    var params= {
                     //         member_id: '',
                     //     }
