aeb8bcf4ebe07b7602251a12885f8e88edbb687f
Cielito <cosmebench@gmail.com>
2017-06-03 00:03:17 +0800

    git push dictionary

--- profiledictionary.js
+++ profiledictionary.js
@@ -6,43 +6,39 @@
     },
     {
         profile: "2",
-        pairs: [1,3,1,1,1]
+        pairs: [1,3,1,1,3]
     },
     {
         profile: "3",
-        pairs: [1,3,1,2,1]
-    },
-    {
-        profile: "3",
-        pairs: [1,3,1,2,1]
+        pairs: [1,3,1,2,3]
     },
     {
         profile: "4",
-        pairs: [1,3,1,2,2]
+        pairs: [1,3,2,2,3]
     },
     {
         profile: "5",
-        pairs: [1,1,2,2,3]
+        pairs: [1,1,2,2,1]
     },
     {
         profile: "6",
-        pairs: [1,1,2,1,3]
-    },
-    {
-        profile: "7",
-        pairs: [1,1,1,2,3]
-    },
-    {
-        profile: "8",
-        pairs: [1,1,1,1,3]
-    },
-    {
-        profile: "9",
         pairs: [1,1,2,1,1]
     },
     {
+        profile: "7",
+        pairs: [1,1,1,2,1]
+    },
+    {
+        profile: "8",
+        pairs: [1,1,1,1,1]
+    },
+    {
+        profile: "9",
+        pairs: [1,1,1,1,2]
+    },
+    {
         profile: "10",
-        pairs: [1,2,1,1,3]
+        pairs: [1,2,1,1,1]
     },
     {
         profile: "11",
@@ -50,7 +46,7 @@
     },
     {
         profile: "12",
-        pairs: [1,3,1,1,2]
+        pairs: [1,3,2,1,3]
     },
     {
         profile: "13",
@@ -62,19 +58,19 @@
     },
     {
         profile: "15",
-        pairs: [1,3,2,1,1]
+        pairs: [1,3,1,2,2]
     },
     {
         profile: "16",
-        pairs: [1,3,1,2,1]
+        pairs: [1,3,1,1,2]
     },
     {
         profile: "17",
-        pairs: [1,2,2,1,1]
+        pairs: [1,2,1,1,2]
     },
     {
         profile: "18",
-        pairs: [1,2,1,2,3]
+        pairs: [1,2,1,2,2]
     },
     {
         profile: "19",
@@ -86,67 +82,67 @@
     },
     {
         profile: "21",
-        pairs: [1,1,1,1,1]
+        pairs: [1,1,1,1,3]
     },
     {
         profile: "22",
-        pairs: [1,1,2,2,1]
-    },
-    {
-        profile: "23",
-        pairs: [1,2,1,1,1]
-    },
-    {
-        profile: "24",
-        pairs: [1,1,1,2,1]
-    },
-    {
-        profile: "25",
-        pairs: [1,1,1,1,2]
-    },
-    {
-        profile: "26",
         pairs: [1,1,1,2,2]
     },
     {
+        profile: "23",
+        pairs: [1,2,1,1,3]
+    },
+    {
+        profile: "24",
+        pairs: [1,1,1,2,3]
+    },
+    {
+        profile: "25",
+        pairs: [1,1,2,1,3]
+    },
+    {
+        profile: "26",
+        pairs: [1,1,2,2,3]
+    },
+    {
         profile: "27",
-        pairs: [1,3,2,2,3]
+        pairs: [1,3,2,2,1]
     },
     {
         profile: "28",
-        pairs: [1,3,2,1,3]
+        pairs: [1,3,2,1,1]
     },
     {
         profile: "29",
-        pairs: [1,3,1,1,3]
+        pairs: [1,3,1,1,1]
     },
     {
         profile: "30",
-        pairs: [1,2,1,2,3]
-    },
-    {
-        profile: "31",
         pairs: [1,2,1,2,1]
     },
     {
+        profile: "31",
+        pairs: [1,2,1,2,3]
+    },
+    {
         profile: "32",
-        pairs: [1,2,1,1,2]
-    },
-    {
-        profile: "33",
-        pairs: [1,2,1,2,2]
-    },
-    {
-        profile: "34",
         pairs: [1,2,2,1,3]
     },
     {
-        profile: "35",
+        profile: "33",
         pairs: [1,2,2,2,3]
     },
     {
+        profile: "34",
+        pairs: [1,2,2,1,1]
+    },
+    {
+        profile: "35",
+        pairs: [1,2,2,2,1]
+    },
+    {
         profile: "36",
-        pairs: [1,3,1,2,3]
+        pairs: [1,3,1,2,1]
     },
     {
         profile: "37",
@@ -154,23 +150,23 @@
     },
     {
         profile: "38",
-        pairs: [2,2,1,2,2]
+        pairs: [2,2,2,2,3]
     },
     {
         profile: "39",
-        pairs: [2,1,1,1,1]
+        pairs: [2,1,1,1,3]
     },
     {
         profile: "40",
-        pairs: [2,1,1,2,1]
+        pairs: [2,1,1,2,3]
     },
     {
         profile: "41",
-        pairs: [2,1,1,1,2]
+        pairs: [2,1,2,1,3]
     },
     {
         profile: "42",
-        pairs: [2,1,1,2,2]
+        pairs: [2,1,2,2,3]
     },
     {
         profile: "43",
@@ -182,67 +178,67 @@
     },
     {
         profile: "45",
-        pairs: [2,2,1,1,1]
+        pairs: [2,2,1,1,3]
     },
     {
         profile: "46",
-        pairs: [2,1,2,2,1]
+        pairs: [2,1,1,2,2]
     },
     {
         profile: "47",
-        pairs: [2,1,2,1,1]
+        pairs: [2,1,1,1,2]
     },
     {
         profile: "48",
-        pairs: [2,3,1,1,1]
+        pairs: [2,3,1,1,3]
     },
     {
         profile: "49",
-        pairs: [2,2,2,2,1]
+        pairs: [2,2,1,2,2]
     },
     {
         profile: "50",
-        pairs: [2,3,1,2,1]
+        pairs: [2,3,1,2,3]
     },
     {
         profile: "51",
-        pairs: [2,3,1,1,2]
+        pairs: [2,3,2,1,3]
     },
     {
         profile: "52",
-        pairs: [2,3,1,2,2]
+        pairs: [2,3,2,2,3]
     },
     {
         profile: "53",
-        pairs: [2,2,1,2,1]
-    },
-    {
-        profile: "54",
-        pairs: [2,2,1,1,2]
-    },
-    {
-        profile: "55",
         pairs: [2,2,1,2,3]
     },
     {
+        profile: "54",
+        pairs: [2,2,2,1,3]
+    },
+    {
+        profile: "55",
+        pairs: [2,2,1,2,1]
+    },
+    {
         profile: "56",
-        pairs: [2,1,1,1,3]
+        pairs: [2,1,1,1,1]
     },
     {
         profile: "57",
-        pairs: [2,1,1,2,3]
+        pairs: [2,1,1,2,1]
     },
     {
         profile: "58",
-        pairs: [2,1,2,1,3]
+        pairs: [2,1,2,1,1]
     },
     {
         profile: "59",
-        pairs: [2,1,2,2,3]
+        pairs: [2,1,2,2,1]
     },
     {
         profile: "60",
-        pairs: [2,2,2,1,3]
+        pairs: [2,2,2,1,1]
     },
     {
         profile: "61",
@@ -254,35 +250,35 @@
     },
     {
         profile: "63",
-        pairs: [2,2,2,2,3]
+        pairs: [2,2,2,2,1]
     },
     {
         profile: "64",
-        pairs: [2,3,2,1,2]
+        pairs: [2,3,1,1,2]
     },
     {
         profile: "65",
-        pairs: [2,3,1,1,3]
+        pairs: [2,3,1,1,1]
     },
     {
         profile: "66",
-        pairs: [2,3,1,2,3]
+        pairs: [2,3,1,2,1]
     },
     {
         profile: "67",
-        pairs: [2,3,2,1,3]
+        pairs: [2,3,2,1,1]
     },
     {
         profile: "68",
-        pairs: [2,3,2,2,3]
+        pairs: [2,3,2,2,1]
     },
     {
         profile: "69",
-        pairs: [2,2,2,1,1]
+        pairs: [2,2,1,1,2]
     },
     {
         profile: "70",
-        pairs: [2,2,1,1,3]
+        pairs: [2,2,1,1,1]
     },
     {
         profile: "71",
@@ -290,63 +286,63 @@
     },
     {
         profile: "72",
-        pairs: [2,3,2,2,1]
+        pairs: [2,3,1,2,2]
     },
     {
         profile: "73",
-        pairs: [3,1,1,1,2]
+        pairs: [3,1,2,1,3]
     },
     {
         profile: "74",
-        pairs: [3,2,1,1,1]
-    },
-    {
-        profile: "75",
         pairs: [3,2,1,1,3]
     },
     {
+        profile: "75",
+        pairs: [3,2,1,1,1]
+    },
+    {
         profile: "76",
-        pairs: [3,2,1,2,3]
-    },
-    {
-        profile: "77",
-        pairs: [3,2,2,1,3]
-    },
-    {
-        profile: "78",
         pairs: [3,2,1,2,1]
     },
     {
+        profile: "77",
+        pairs: [3,2,2,1,1]
+    },
+    {
+        profile: "78",
+        pairs: [3,2,1,2,3]
+    },
+    {
         profile: "79",
-        pairs: [3,1,1,2,2]
+        pairs: [3,1,2,2,3]
     },
     {
         profile: "80",
-        pairs: [3,1,1,1,3]
-    },
-    {
-        profile: "81",
-        pairs: [3,1,1,2,1]
-    },
-    {
-        profile: "82",
         pairs: [3,1,1,1,1]
     },
     {
+        profile: "81",
+        pairs: [3,1,1,2,3]
+    },
+    {
+        profile: "82",
+        pairs: [3,1,1,1,3]
+    },
+    {
         profile: "83",
-        pairs: [3,3,1,1,3]
+        pairs: [3,3,1,1,1]
     },
     {
         profile: "84",
-        pairs: [3,3,1,2,3]
+        pairs: [3,3,1,2,1]
     },
     {
         profile: "85",
-        pairs: [3,3,2,1,3]
+        pairs: [3,3,2,1,1]
     },
     {
         profile: "86",
-        pairs: [3,2,2,2,3]
+        pairs: [3,2,2,2,1]
     },
     {
         profile: "87",
@@ -354,11 +350,11 @@
     },
     {
         profile: "88",
-        pairs: [3,2,2,1,1]
+        pairs: [3,2,1,1,2]
     },
     {
         profile: "89",
-        pairs: [3,2,2,2,1]
+        pairs: [3,2,1,2,2]
     },
     {
         profile: "90",
@@ -366,11 +362,11 @@
     },
     {
         profile: "91",
-        pairs: [3,2,2,1,2]
+        pairs: [3,2,2,2,2]
     },
     {
         profile: "92",
-        pairs: [3,1,2,1,2]
+        pairs: [3,1,2,2,2]
     },
     {
         profile: "93",
@@ -378,23 +374,23 @@
     },
     {
         profile: "94",
-        pairs: [3,1,2,2,1]
+        pairs: [3,1,1,2,2]
     },
     {
         profile: "95",
-        pairs: [3,1,2,1,1]
+        pairs: [3,1,1,1,2]
     },
     {
         profile: "96",
-        pairs: [3,1,2,1,3]
+        pairs: [3,1,2,1,1]
     },
     {
         profile: "97",
-        pairs: [3,3,2,2,1]
+        pairs: [3,3,1,2,2]
     },
     {
         profile: "98",
-        pairs: [3,2,1,1,2]
+        pairs: [3,2,2,1,3]
     },
     {
         profile: "99",
@@ -402,39 +398,39 @@
     },
     {
         profile: "100",
-        pairs: [3,3,1,2,2]
-    },
-    {
-        profile: "101",
-        pairs: [3,3,1,1,2]
-    },
-    {
-        profile: "102",
-        pairs: [3,3,1,2,1]
-    },
-    {
-        profile: "103",
-        pairs: [3,3,1,1,1]
-    },
-    {
-        profile: "104",
         pairs: [3,3,2,2,3]
     },
     {
+        profile: "101",
+        pairs: [3,3,2,1,3]
+    },
+    {
+        profile: "102",
+        pairs: [3,3,1,2,3]
+    },
+    {
+        profile: "103",
+        pairs: [3,3,1,1,3]  
+    },
+    {
+        profile: "104",
+        pairs: [3,3,2,2,1]
+    },
+    {
         profile: "105",
-        pairs: [3,1,1,2,3]
+        pairs: [3,1,1,2,1]
     },
     {
         profile: "106",
-        pairs: [3,1,2,2,3]
+        pairs: [3,1,2,2,1]
     },
     {
         profile: "107",
-        pairs: [3,2,1,1,2]
+        pairs: [3,2,2,2,3]
     },
     {
         profile: "108",
-        pairs: [3,2,2,1,1]
+        pairs: [3,3,1,1,2]
     }
 ]
 
