9c92bf6d308106ad709919aa78d6dbdd19d7a01d
Shin <shin@chatbot.ph>
2017-06-05 14:37:54 +0800

    test

--- dialogs/onboarding-1stpart.js
+++ dialogs/onboarding-1stpart.js
@@ -14,7 +14,7 @@ module.exports = [
         session.dialogData.user = args.user === undefined ? "" : args.user;
         session.beginDialog('/member-session', session.dialogData);
 
-        console.log('if retake? ', session);
+        console.log('if retake? ', session.message.text);
 
     },
 
