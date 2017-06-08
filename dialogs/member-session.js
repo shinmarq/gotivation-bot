c152cfe7bc55cb681101eb698cbdd41ca33f3a42
Shin <shin@chatbot.ph>
2017-06-08 11:59:38 +0800

    test

--- dialogs/member-session.js
+++ dialogs/member-session.js
@@ -10,6 +10,8 @@ module.exports = [
         var params = {
             memberId: session.message.address.user.id
         }
+
+        console.log('check category =>', category);
         parser.member.getmember(params, function (error, response, getbody) {
             if (!error && response.statusCode == 200) {
                 membercategory = getbody.categories;
