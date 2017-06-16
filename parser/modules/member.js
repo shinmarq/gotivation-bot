2983abc2428f0a7404be49c9717c2f793726fa88
shinmarq <shin@chatbot.ph>
2017-06-16 19:03:14 +0800

    test

--- parser/modules/member.js
+++ parser/modules/member.js
@@ -24,7 +24,7 @@ exports.getmember = function (params, callback) {
 	}
 
 	request.get(options, function (err, res, body) {
-		console.log('OPTIONS', options);
+		console.log('response from getmember parser', options);
 		console.log('REQUEST GETMEMBER FROM PARSER', body);
 		if (err == null && res.statusCode == constants.SUCCESS) {
 			var mapResponse = new MapResponse(body);
