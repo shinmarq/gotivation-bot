d6a28087df10999f3d30d65c364b04eb245b020b
shinmarq <shin@chatbot.ph>
2017-06-16 14:04:27 +0800

    test

--- dialogs/unsubscribe.js
+++ dialogs/unsubscribe.js
@@ -18,8 +18,8 @@ module.exports = [
             parser.member.updatemember(params, function (err, res, body) {
                 if(!err){
                     console.log(res.statusCode);
-                console.log('success unsubscribed');
-                session.endConversation('Successfully unsubscibed. If you want to get started again with those motivations, just type "Start over"');
+                    console.log('success unsubscribed');
+                    session.endConversation('Successfully unsubscibed. If you want to get started again with those motivations, just type "Start over"');
                 }else{console.log('error occur in unsubscribing')}
             });
         } else {
