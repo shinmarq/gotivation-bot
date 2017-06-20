5eb1d5ad775004822a5baad6c9dc34c9c583bb48
bench cosme <cosmebench@gmail.com>
2017-06-20 20:18:40 +0800

    add quote

--- dialogs/validatecoach.js
+++ dialogs/validatecoach.js
@@ -24,7 +24,6 @@ module.exports =
                     }
                     //uncomment this is how to get code
                     parser.users.getuser(params, function (error, statusCode, body) {
-                        console.log(body);
                         if (!error && body.length > 0) {
                             var result = _.findWhere(body, { coach_code: params.coach_code });
                             session.dialogData.coach._id = result._id;
