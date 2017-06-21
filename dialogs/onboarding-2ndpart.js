fc38e3638a499a52f960c9ddf1c77d87c3266561
bench cosme <cosmebench@gmail.com>
2017-06-21 16:09:44 +0800

    console log

--- dialogs/onboarding-2ndpart.js
+++ dialogs/onboarding-2ndpart.js
@@ -54,6 +54,7 @@ module.exports = [
                 session.sendTyping();
                 parser.member.updatemember(params, function (err, res, body) {
                     if (!err && res.statusCode == 200) {
+                        console.log(body.profiletype);
                         if (body.profiletype == '' || body.profiletype == undefined || body.profiletype == null) {
                             session.replaceDialog('/onboarding-3rdpart',body.onboarded);
                         }
